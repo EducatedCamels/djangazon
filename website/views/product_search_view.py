5b129f850e892e44ff46b3a51169fd7ee316a04f
Dean Smith <Dean Smith>
2017-06-02 10:24:52 -0500

    Added to search product function. Changed cities in fixtures

--- website/views/product_search_view.py
+++ website/views/product_search_view.py
@@ -7,11 +7,11 @@ from website.models import Product
 
 def product_search(request):
 
-    all_products = Product.objects.all().order_by("title")
+    all_products = Product.objects.all().order_by("title", "city")
     query = request.GET.get("q")
     template_name = 'product/product_search.html'
     if query:
         products = all_products.filter(
-        Q(title__contains=query)
+        Q(title__contains=query) | Q(city__contains=query)
         ).distinct()
         return render(request, template_name, {'all_products': products})
